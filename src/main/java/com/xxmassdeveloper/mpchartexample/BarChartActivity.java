0630dd636999d69d99201ff843a87e7ea8137bcb
Your Name <you@example.com>
2023-06-27 18:52:59 +0300

    ASASA

--- src/main/java/com/xxmassdeveloper/mpchartexample/BarChartActivity.java
+++ src/main/java/com/xxmassdeveloper/mpchartexample/BarChartActivity.java
@@ -66,7 +66,9 @@ public class BarChartActivity extends DemoBase implements OnSeekBarChangeListene
         setTitle("BarChartActivity");
         setTitle("BarChartActivity");
         setTitle("BarChartActivity");
-
+        setTitle("BarChartActivity");
+        setTitle("BarChartActivity");
+        setTitle("BarChartActivity");
         tvX = findViewById(R.id.tvXMax);
         tvY = findViewById(R.id.tvYMax);
 
